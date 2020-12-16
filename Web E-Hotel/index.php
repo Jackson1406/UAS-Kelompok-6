37fdd3ed35bc54d536037d421df382cffcee52eb
TekproKel6-1SIMA <76107139+TekproKel6-1SIMA@users.noreply.github.com>
2020-12-16 19:49:59 +0700

    Add files via upload

---
+++ Web E-Hotel/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fauna+One&display=swap" rel="stylesheet">
+
+    <title>E-Hotel</title>
+    <link rel="stylesheet" href="required.css">
+    <link rel="stylesheet" href="indexStyle.css" >
+    <link rel="icon" href="logo.png" type="image/x-icon">
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <div class="logo">
+                <h4>E-Hotel</h4>
+            </div>
+
+            <ul>
+                <li><a href="#" class="home">Home</a></li>
+                <li><a href="photo/photo.php" class="photo">Photos</a></li>
+                <li><a href="room/room.php" class="room">Rooms</a>
+                    <ul class="dropbar">
+                        <li><a href="room/room.php#deluxe">Deluxe Room</a></li>
+                        <li><a href="room/room.php#family">Family Room</a></li>
+                        <li><a href="room/room.php#suite">Suite Room</a></li>
+                    </ul>
+                </li>
+                <li><a href="about/about.php" class="about">About</a></li>
+            </ul>
+
+        </nav>
+    </div>
+
+    <div class="body1">
+        <p>E-HOTEL</p>
+        <span class="bookbutton"><a href="book/booking.php">BOOK NOW</a></span>
+    </div>
+
+    <div class="body2">
+        <h1>WELCOME TO E-HOTEL</h1>
+        <h2>Enjoy your vacation, and stay comfortably at our hotel.</h2>
+        <p>Dear Guest, We are delighted that you have selected our hotel. On behalf of the entire team at the Hotel Name, I extend you a very warm welcome and trust your stay with us will be both enjoyable and comfortable.</p>
+
+    </div>
+
+    <div class="body3">
+        <div class="b3p1">
+            <div class="desc3">
+                <h2>Dear Guest,</h2>
+                <p>It is our pleasure to welcome you to E-Hotel. Whether you are travelling on business or pleasure, we know you have a variety of hotels to choose from. We strive to provide you with a room that is clean and comfortable, with friendly attentive service at a value-conscious price.</p>
+            </div>
+            <div class="foto3"></div>
+        </div>
+    
+        <div class="b3p2">
+            <img src="imgIndex/foto4.jpg" alt="foto4">
+            <img src="imgIndex/foto5.jpg" alt="foto5">
+        </div>
+    </div>
+
+    
+    <div class="body4">
+        <div class="b4p1">
+            <h1>AN ICONIC LOCATION IN THE WORLD</h1>
+            <hr>
+        </div>
+
+        <div class="b4p2">
+            <div class="foto4"></div>
+            <div class="desc4">
+                <h2>ENJOY OUR LUXURIOUS VIEW</h2>
+                <P>With something that beautiful, you know it's one of those "take a picture, it'll last longer" moments. "I love places that make you realize how tiny you and your problems are."</P>
+            </div>
+            
+            <div class="clear"></div>
+        </div>
+
+        <div class="b4p3">
+            <img src="imgIndex/casino1.jpg" alt="">
+            <img src="imgIndex/loby1.jpg" alt="">
+        </div>
+    </div>
+
+
+    <div class="body5">
+        <div class="desc5">
+            <h2>MEMORABLE DINING EXPERIENCE</h2>
+            <hr>
+            <p>One of the very nicest things about life is the way we must regularly stop whatever it is we are doing and devote our attention to eating.</p>
+    
+        </div>
+    </div>
+
+
+    <div class="body6">
+        <div class="b6p1">
+            <div class="foto6"></div>
+            <div class="desc6">
+                <h2>EXCEPTIONAL TASTE OF FOOD</h2>
+                <P>Offers a myriad of flavors, including modern Indian, authentic Thai, social Japanese, delicious Italian cuisines and prime steaks.</P>
+            </div>
+            
+            <div class="clear"></div>
+        </div>
+
+        <div class="b6p2">
+            <img src="imgIndex/restr2.jpg" alt="">
+            <img src="imgIndex/restr3.jpg" alt="">
+        </div>
+    </div>
+
+
+    <div class="body7">
+        <div class="b7p1">
+            <div class="foto7"></div>
+            <div class="desc7">
+                <h2>MEMORABLE ENTERTAIMENT</h2>
+                <P>In all of living, have much fun and laughter. Life is to be enjoyed, not just endured. You can enjoy it here at E-Hotel.</P>
+            </div>
+            
+            <div class="clear"></div>
+        </div>
+
+        <div class="b7p2">
+            <img src="imgIndex/karaoke1.jpg" alt="">
+            <img src="imgIndex/club1.jpg" alt="">
+        </div>
+
+
+    </div>
+
+
+    <div class="footer">
+        <div class="atas">
+            <div class="street">
+                <h3>E-Hotel</h3>
+                <span>Country , Road , street</span>
+                <span>+812 5566 8888 | ehotel@gmail.con</span>
+            </div>
+
+            <h4>E-Hotel</h4>
+
+            <div class="connect">
+                <span>Connect With Us</span>
+                <ul>
+                    <li><a href="https://www.facebook.com/" target="_blank"><img src="imgIndex/fb1.png" alt="" class="fb"></a></li>
+                    <li><a href="https://www.instagram.com/" target="_blank"><img src="imgIndex/ig1.png" alt="" class="ig"></a></li>
+                    <li><a href="https://twitter.com/?lang=en" target="_blank"><img src="imgIndex/tweet1.png" alt="" class="tweet"></a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="bawah">
+            <p>2020 E-Hotel. All Rights Reserved.</p>
+        </div>
+    </div>
+
+</body>
+
+
+</html>
